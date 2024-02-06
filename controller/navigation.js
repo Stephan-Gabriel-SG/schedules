e7852d44bc43cbbacea18e434cb460ce92978b68
Nandrasantsoa Stephan Gabriel <stephangabrielnandrasantsoa@gmail.com>
2024-02-06 15:37:20 +0100

    update navigation

--- controller/navigation.js
+++ controller/navigation.js
@@ -6,11 +6,12 @@ navRouter.get('/', (req, res, next) => {
 navRouter.get('/addPlan', (req, res, next) => {
   res.render('pages/addForm.ejs', {
     inputs: [
+      { label: 'MODULE', type: 'text' },
       { label: 'PROF', type: 'text' },
-      { label: 'SALLE', type: 'text' },
       { label: 'CREDIT', type: 'number' },
       { label: 'NIVEAU', type: 'text' },
       { label: 'DATE', type: 'date' },
+      { label: 'SALLE', type: 'text' },
     ],
   })
 })
