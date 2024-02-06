36152ddea37a60a206070fd4a90cdff7d740cfde
Nandrasantsoa Stephan Gabriel <stephangabrielnandrasantsoa@gmail.com>
2024-02-06 17:45:53 +0100

    update model

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const navRouter = require('./controller/navigation')
+const scheduleRoute = require('./controller/schedules')
 const app = express()
 app.use(express.json())
 app.use(express.static(__dirname + '/views/styles'))
 app.set('views engine', 'ejs')
 app.use('/', navRouter)
+app.use('/api/schedules', scheduleRoute)
 app.use((req, res, next) => {
   res.status(404).send({ error: 'page not found' })
 })
--- model/Schedule.js
+++ model/Schedule.js
@@ -1,4 +1,37 @@
-const sequelize = new Sequelize('database', 'username', 'password', {
+const { Sequelize, DataTypes } = require('@sequelize/core')
+
+const sequelize = new Sequelize('weeklyschedule', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
 })
+
+const Schedule = sequelize.define(
+  'Schedule',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    module: {
+      type: DataTypes.STRING,
+    },
+    prof: {
+      type: DataTypes.STRING,
+    },
+    salle: {
+      type: DataTypes.STRING,
+    },
+    credit: {
+      type: DataTypes.DECIMAL,
+    },
+    niveau: {
+      type: DataTypes.STRING,
+    },
+    date_: {
+      type: DataTypes.DATE,
+    },
+  },
+  { tableName: 'schedule' }
+)
+module.exports = { Schedule, sequelize }
