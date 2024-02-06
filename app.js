4d3d6601fd5d2e726124d0cd5abeffc403bf4ef0
Nandrasantsoa Stephan Gabriel <stephangabrielnandrasantsoa@gmail.com>
2024-02-06 13:13:59 +0100

    add accueil.ejs pages

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res, next) => {
+  res.render('accueil')
+})
+
 app.use((req, res, next) => {
   res.status(404).send({ error: 'page not found' })
 })
+
 module.exports = app
