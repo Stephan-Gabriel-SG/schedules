bf63f5f38c7c11612d0fc13dd80a257d30528ee6
Nandrasantsoa Stephan Gabriel <stephangabrielnandrasantsoa@gmail.com>
2024-02-07 18:15:11 +0100

    update methode put in schedules.js

--- controller/schedules.js
+++ controller/schedules.js
@@ -35,7 +35,7 @@ scheduleRoute.delete('/delete/:id', async (req, res, next) => {
 scheduleRoute.put('/update/:id', async (req, res, next) => {
   console.log('body:', req.body)
   const body = req.body
-  Schedule.update({ body }, { where: { id: req.params.id } })
+  Schedule.update(body, { where: { id: req.params.id } })
     .then((result) => res.status(200).send({ success: true, record: result }))
     .catch((error) => res.status(500).send({ error }))
 })
